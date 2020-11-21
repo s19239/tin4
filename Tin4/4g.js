6873582eaacc0d2e957c3a0cfe410af357fd7986
s19239 <61867891+s19239@users.noreply.github.com>
2020-11-22 02:30:04 +0600

    Add files via upload

---
+++ Tin4/4g.js
@@ -0,0 +1,20 @@
+function type(value)
+{
+var types = [Function, RegExp, Number, String, Boolean, Object], x, len;
+    
+if (typeof value === "object" || typeof value === "function") 
+    {
+     for (x = 0, len = dtypes.length; x < len; x++) 
+     {
+            if (value instanceof dtypes[x])
+            {
+                return dtypes[x];
+            }
+      }
+    }
+    
+    return typeof value;
+}
+console.log(type(12));
+console.log(type("Hello"));
+console.log(type(true));
